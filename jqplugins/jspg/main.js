35e60ba65c34449fbf3774d6f70708c17af9f7e9
Daniel Macedo Gomes <dmacgos@gmail.com>
2024-04-01 11:33:48 -0300

    commit inicial

---
+++ jqplugins/jspg/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('#cpf').mask('000.000.000-00');
+
+    $('#cep').mask('00000-000');
+
+    $('form-campo').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            telefone: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            cep: {
+                required: true,
+            },
+            endereco: {
+                required: true,
+            },
+        },
+            messages: {
+                nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form) {
+            console.log
+        },
+            invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
+
